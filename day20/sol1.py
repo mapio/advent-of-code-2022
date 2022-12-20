2e12be3fd6428b36edd3e744b216b2e1528048a9
Massimo Santini <santini@di.unimi.it>
2022-12-21 00:21:50 +0100

    Day 20

---
+++ day20/sol1.py
@@ -0,0 +1,42 @@
+from dataclasses import dataclass
+from itertools import starmap 
+from pathlib import Path
+
+@dataclass
+class PosVal:
+  pos: int
+  val: int
+  @classmethod
+  def fromiterable(cls, it):
+    return list(starmap(cls, enumerate(it)))
+
+def find(pv, pos):
+  for pv in pv: 
+    if pv.pos == pos: return pv
+
+def find0(pv):
+  for ipv in PV:
+    if ipv.val == 0: return ipv.pos
+
+def move(pv, idx):
+  lpv = pv[idx]
+  pin = lpv.pos
+  pfi = 1 + (pin + lpv.val - 1) % (len(pv) - 1)
+  #print(lpv, pin, pfi)
+  if pfi < pin:
+    for ipv in pv:
+      if pfi <= ipv.pos < pin: ipv.pos += 1
+  elif pfi > pin:
+    for ipv in pv:
+      if pin < ipv.pos <= pfi: ipv.pos -= 1
+  else:
+    return
+  lpv.pos = pfi
+  return pv
+
+V = list(map(int, Path('input.txt').read_text().splitlines()))
+PV = PosVal.fromiterable(V)
+for idx in range(len(PV)): move(PV, idx)
+idx = find0(PV)
+print(find(PV, (idx + 1000) % len(PV)).val + find(PV, (idx + 2000) % len(PV)).val + find(PV, (idx + 3000) % len(PV)).val)
+
